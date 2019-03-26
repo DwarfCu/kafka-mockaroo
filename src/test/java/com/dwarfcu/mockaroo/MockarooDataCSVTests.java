ec02a222464e538898b18da31ad38773abcd797d
jose.hervas <jose.hervas@treelogic.com>
2019-03-26 15:05:19 +0100

    new MockarooData constructor and added test.

--- src/test/java/com/dwarfcu/mockaroo/MockarooDataCSVTests.java
+++ src/test/java/com/dwarfcu/mockaroo/MockarooDataCSVTests.java
@@ -3,13 +3,23 @@ package com.dwarfcu.mockaroo;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MockarooDataCSVTests {
+import java.util.Properties;
 
-  private MockarooData mockarooData = new MockarooData();
+public class MockarooDataCSVTests {
 
   @Test
   public void getCSVDataset() {
+    MockarooData mockarooData = new MockarooData();
     Assert.assertEquals(30, mockarooData.getDataString().length);
   }
 
+  @Test
+  public void getJSONDataset() {
+    Properties properties = new Properties();
+    properties.setProperty("mockaroo.format", "json");
+    properties.setProperty("mockaroo.url", "https://api.mockaroo.com/api/9b0d87e0?count=1000&key=9ae0b0d0");
+
+    MockarooData mockarooData = new MockarooData(properties);
+    Assert.assertEquals(1000, mockarooData.getDataJson().length());
+  }
 }
